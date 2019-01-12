df5ca2a47aeef0783fea6852510b9296590b66ac
Daniel Dopierała <dopierala.daniel@gmail.com>
2019-01-12 21:30:33 +0100

    Import project from Spring Initializr

---
+++ src/main/java/pl/dopieralad/university/sk2/irc/client/IrcClientApplication.java
@@ -0,0 +1,14 @@
+package pl.dopieralad.university.sk2.irc.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrcClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IrcClientApplication.class, args);
+	}
+
+}
+

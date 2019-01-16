e7d9a17f6d6665d41d2bdae9c6f772819875dc1d
Daniel Dopierała <daniel.dopierala@snpgroup.com>
2019-01-16 08:19:35 +0100

    Fix Vaadin + Spring scope integration

--- src/main/java/pl/dopieralad/university/sk2/irc/client/ui/MainView.java
+++ src/main/java/pl/dopieralad/university/sk2/irc/client/ui/MainView.java
@@ -2,17 +2,13 @@ package pl.dopieralad.university.sk2.irc.client.ui;
 
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.spring.annotation.SpringComponent;
-import org.springframework.beans.factory.annotation.Autowired;
+import pl.dopieralad.university.sk2.irc.client.ui.control.MessageDisplay;
 import pl.dopieralad.university.sk2.irc.client.ui.control.MessageSender;
 
-@SpringComponent
 @Route
 public class MainView extends VerticalLayout {
 
-    @Autowired
-    public MainView(MessageSender messageSender) {
-        add(messageSender);
+    public MainView(MessageDisplay messageDisplay, MessageSender messageSender) {
+        add(messageDisplay, messageSender);
     }
-
 }
--- src/main/java/pl/dopieralad/university/sk2/irc/client/ui/control/MessageSender.java
+++ src/main/java/pl/dopieralad/university/sk2/irc/client/ui/control/MessageSender.java
@@ -10,11 +10,13 @@ import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.spring.annotation.SpringComponent;
+import com.vaadin.flow.spring.annotation.UIScope;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import pl.dopieralad.university.sk2.irc.server.ServerAdapter;
 
 @SpringComponent
+@UIScope
 public class MessageSender extends HorizontalLayout {
 
     private final transient Logger logger;

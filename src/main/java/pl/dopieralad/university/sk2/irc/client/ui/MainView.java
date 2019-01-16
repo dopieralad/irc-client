aff6122a42e30bdbdbdeb601dbfc489cf3c4bd50
Daniel Dopierała <daniel.dopierala@snpgroup.com>
2019-01-16 21:47:27 +0100

    Set page title to "IRC"

--- src/main/java/pl/dopieralad/university/sk2/irc/client/ui/MainView.java
+++ src/main/java/pl/dopieralad/university/sk2/irc/client/ui/MainView.java
@@ -1,11 +1,13 @@
 package pl.dopieralad.university.sk2.irc.client.ui;
 
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import pl.dopieralad.university.sk2.irc.client.ui.control.MessageDisplay;
 import pl.dopieralad.university.sk2.irc.client.ui.control.MessageSender;
 
 @Route
+@PageTitle("IRC")
 public class MainView extends VerticalLayout {
 
     public MainView(MessageDisplay messageDisplay, MessageSender messageSender) {

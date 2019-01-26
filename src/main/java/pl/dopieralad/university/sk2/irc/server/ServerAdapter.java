aa9afa8d35362aebbc4aeee6fab66b36b289ddf9
Daniel Dopierała <daniel.dopierala@snpgroup.com>
2019-01-26 13:11:24 +0100

    Renew connection on Socket disconnect

--- src/main/java/pl/dopieralad/university/sk2/irc/server/ServerAdapter.java
+++ src/main/java/pl/dopieralad/university/sk2/irc/server/ServerAdapter.java
@@ -1,23 +1,28 @@
 package pl.dopieralad.university.sk2.irc.server;
 
 import java.io.IOException;
+import java.net.SocketException;
 import java.nio.charset.StandardCharsets;
+import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.scheduling.annotation.Async;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 import org.springframework.util.unit.DataSize;
 
 @Component
 public class ServerAdapter {
 
-    private final ServerSocket serverSocket;
+    private final ApplicationContext applicationContext;
     private final int headerLength;
 
-    public ServerAdapter(ServerSocket serverSocket, @Value("#{serverProperties.headerLength}") DataSize headerLength) {
-        this.serverSocket = serverSocket;
+    private ServerSocket serverSocket;
+    private boolean isShuttingDown = false;
+
+    public ServerAdapter(ApplicationContext applicationContext, @Value("#{serverProperties.headerLength}") DataSize headerLength) {
+        this.applicationContext = applicationContext;
         this.headerLength = Math.toIntExact(headerLength.toBytes());
     }
 
@@ -30,22 +35,30 @@ public class ServerAdapter {
         outputStream.write(bytes);
     }
 
-    @Async
-    public void sendAsync(String content) throws IOException {
-        send(content);
+    public String receive() throws IOException {
+        try {
+            var inputStream = serverSocket.getInputStream();
+            var headerBytes = inputStream.readNBytes(headerLength);
+            var header = new String(headerBytes);
+            var contentLength = Integer.parseInt(header);
+            var bytes = inputStream.readNBytes(contentLength);
+            return StringUtils.toEncodedString(bytes, StandardCharsets.UTF_8);
+        } catch (NumberFormatException | SocketException e) {
+            restart();
+            return receive();
+        }
     }
 
-    public String receive() throws IOException {
-        var inputStream = serverSocket.getInputStream();
-        var headerBytes = inputStream.readNBytes(headerLength);
-        var header = new String(headerBytes);
-        var contentLength = Integer.parseInt(header);
-        var bytes = inputStream.readNBytes(contentLength);
-        return StringUtils.toEncodedString(bytes, StandardCharsets.UTF_8);
+    @PostConstruct
+    private void restart() {
+        if (!isShuttingDown) {
+            this.serverSocket = applicationContext.getBean(ServerSocket.class);
+        }
     }
 
     @PreDestroy
-    public void shutdown() throws IOException {
+    private void shutdown() throws IOException {
+        isShuttingDown = true;
         send("/disconnect");
     }
 }
--- src/main/java/pl/dopieralad/university/sk2/irc/server/ServerSocket.java
+++ src/main/java/pl/dopieralad/university/sk2/irc/server/ServerSocket.java
@@ -4,9 +4,12 @@ import java.io.IOException;
 import java.net.Socket;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
 public class ServerSocket extends Socket {
 
     @Autowired
